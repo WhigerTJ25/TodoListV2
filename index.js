acb21c02b4f68adb530061c752935d74da97b0e7
WhigerTJ25 <thejus825@gmail.com>
2023-08-28 22:44:11 +0530

    2n commit

--- index.js
+++ index.js
@@ -160,6 +160,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.POST, function() {
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
